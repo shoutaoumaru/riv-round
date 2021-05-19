d6a7187add0545aba50f49e4376d7efce5aaa6ed
shota.oumaru <otokmae.black@gmail.com>
2021-05-19 20:30:07 +0900

    mobilemenu

--- scripts/libs/svg-animation.js
+++ scripts/libs/svg-animation.js
@@ -1,11 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   new Vivus('move', {
-//     type: 'scenario-sync',
-//     duration: 200,
-//     forceRender: false,
-//     animTimingFunction: Vivus.EASE
-//   })
-// });
 new Vivus('move', {
   type: 'scenario-sync',
   duration: 200,
