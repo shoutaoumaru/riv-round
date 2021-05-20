6b21021776877866676ddc7eac39627b8e8a7370
shota.oumaru <otokmae.black@gmail.com>
2021-05-20 13:51:59 +0900

    opening-pace.js

--- scripts/libs/svg-animation.js
+++ scripts/libs/svg-animation.js
@@ -1,6 +1,14 @@
-new Vivus('move', {
-  type: 'scenario-sync',
-  duration: 200,
-  forceRender: false,
-  animTimingFunction: Vivus.EASE
-})
+// new Vivus('move', {
+//   type: 'scenario-sync',
+//   duration: 200,
+//   forceRender: false,
+//   animTimingFunction: Vivus.EASE
+// })
+document.addEventListener('DOMContentLoaded', function () {
+  new Vivus('move', {
+    type: 'scenario-sync',
+    duration: 200,
+    forceRender: false,
+    animTimingFunction: Vivus.EASE
+  });
+});
